45b710c98053c045a62f0d13b38242d0a6489a14
BaeInHoo <qodlsgh0210@naver.com>
2022-09-18 09:07:08 +0900

    상품 리스트 보여주기

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 export default function Header() {
   return (
     <header>
-      <div className="header_container">
+      <div className="header__container">
         <div className="title">Awesome Prototypes in Shop</div>
         <div className="subtitle">
           Check out what other designers have created using ProtoPie-download
           these examples to learn exactly how they made their interactions.
         </div>
-        <div className="btn_area">
+        <div className="btn__area">
           <a href="https://www.protopie.io" target="_BLANK" rel="noreferrer">
             <button>Try ProtoPie Yourself</button>
           </a>
